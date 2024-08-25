bfcaca9b5a60a764037de86101f9427a7550b56a
Sanjana Anty <sanjana.anty@gmail.com>
2024-08-25 21:32:27 +0600

    Added Aurora Background

---
+++ src/components/AuroraBackground.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AuroraBackground() {
+  return <div>AuroraBackground</div>;
+}
+
+export default AuroraBackground;
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,6 +5,9 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
 
+import { motion } from "framer-motion";
+import { AuroraBackground } from "./ui/aurora-background";
+
 export function SignUpForm() {
   const [formData, setFormData] = useState({
     userName: "",
@@ -30,61 +33,69 @@ export function SignUpForm() {
   };
 
   return (
-    <div className="min-h-dvh flex">
-      <div className="opacity-80 max-sm:hidden sm:w-[360px] lg:w-[600px]">
-        <img src="/images/1.jpg" alt="" />
-      </div>
-      <div className="max-w-md w-full mx-auto  rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-        <h1 className="font-bold text-center text-xl text-neutral-800 dark:text-neutral-300">
-          Welcome to E-commerce Website
-        </h1>
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="max-w-md w-full mx-auto  rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+          <h1 className="font-bold text-center text-xl text-neutral-800 dark:text-neutral-300">
+            Welcome to E-commerce Website
+          </h1>
 
-        <form className="my-8" onSubmit={handleSubmit}>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="user">User Name</Label>
-            <Input
-              name="userName"
-              placeholder="Tyler"
-              type="text"
-              value={formData.userName}
-              onChange={handleChange}
-            />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              name="password"
-              placeholder="••••••••"
-              type="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </LabelInputContainer>
-          <button
-            className="mt-8 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
-            Log in &rarr;
-            <BottomGradient />
-          </button>
-
-          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-          <div className="flex flex-col space-y-4">
-            <h3 className="font-bold text-center text-xl text-neutral-800 dark:text-neutral-200">
-              Don&apos;t have an account?
-            </h3>
+          <form className="my-8" onSubmit={handleSubmit}>
+            <LabelInputContainer className="mb-4">
+              <Label htmlFor="user">User Name</Label>
+              <Input
+                name="userName"
+                placeholder="Tyler"
+                type="text"
+                value={formData.userName}
+                onChange={handleChange}
+              />
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-4">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                name="password"
+                placeholder="••••••••"
+                type="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </LabelInputContainer>
             <button
-              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              className="mt-8 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
               type="submit"
             >
-              Sign up &rarr;
+              Log in &rarr;
               <BottomGradient />
             </button>
-          </div>
-        </form>
-      </div>
-    </div>
+
+            <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+            <div className="flex flex-col space-y-4">
+              <h3 className="font-bold text-center text-xl text-neutral-800 dark:text-neutral-200">
+                Don&apos;t have an account?
+              </h3>
+              <button
+                className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                type="submit"
+              >
+                Sign up &rarr;
+                <BottomGradient />
+              </button>
+            </div>
+          </form>
+        </div>
+      </motion.div>
+    </AuroraBackground>
   );
 }
 
