cf7959adf737c4d80bb2d1a261d58eda252b7ff7
Sanjana Anty <sanjana.anty@gmail.com>
2024-09-02 14:43:51 +0600

    Delete API integrated

--- src/components/ThreeDCard.jsx
+++ src/components/ThreeDCard.jsx
@@ -1,39 +1,19 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export function ThreeDCardDemo({ id }) {
   const [product, setProduct] = useState([]);
+  const getProductDataURL = `https://dummyjson.com/products/${id}`;
+  const router = useRouter();
 
-  //API for getting single product
-  const fetchProduct = async () => {
-    const getProductDataURL = `https://dummyjson.com/products/${id}`;
-
+  const handleClick = async () => {
     try {
-      const response = await fetch(getProductDataURL);
-
-      if (!response.ok) {
-        throw new Error(
-          `Network response was not ok. Status: ${response.status}`
-        );
-      }
-
-      const productData = await response.json();
-      // console.log(productData);
-      setProduct(productData || []);
-    } catch (error) {
-      console.error("Error occurred during fetching products:", error);
-    }
-  };
-  fetchProduct();
-
-  const handleClick = () => {
-    const getProductDataURL = `https://dummyjson.com/products/${id}`;
-    try {
-      const response = fetch(getProductDataURL, {
+      const response = await fetch(getProductDataURL, {
         method: "DELETE",
       });
 
@@ -43,30 +23,51 @@ export function ThreeDCardDemo({ id }) {
         );
       }
 
-      const deletedProductData = response.json();
-      console.log(deletedProductData);
+      const deletedProductData = await response.json();
+
+      if (deletedProductData?.isDeleted) {
+        alert(
+          `${deletedProductData.title} Deleted on ${deletedProductData.deletedOn}!!`
+        );
+        router.push("/products");
+      }
     } catch (error) {
       console.error("Error occurred during fetching products:", error);
     }
   };
 
+  useEffect(() => {
+    //API for getting single product
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(getProductDataURL);
+
+        if (!response.ok) {
+          throw new Error(
+            `Network response was not ok. Status: ${response.status}`
+          );
+        }
+
+        const productData = await response.json();
+        setProduct(productData || []);
+      } catch (error) {
+        console.error("Error occurred during fetching products:", error);
+      }
+    };
+    fetchProduct();
+  }, [getProductDataURL, id]);
+
   const productImage = product?.thumbnail;
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-        <CardItem
-          translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
-        >
+        <CardItem className="text-xl font-bold text-neutral-600 dark:text-white">
           {product?.title}
         </CardItem>
-        <CardItem
-          translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-        >
+        <CardItem className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
           {product?.description}
         </CardItem>
-        <CardItem translateZ="100" className="w-full mt-4">
+        <CardItem className="w-full mt-4">
           <Image
             src={productImage}
             height={300}
@@ -77,7 +78,6 @@ export function ThreeDCardDemo({ id }) {
         </CardItem>
         <div className="flex justify-between items-center mt-20">
           <CardItem
-            translateZ={20}
             as={Link}
             href="https://twitter.com/mannupaaji"
             target="__blank"
@@ -87,7 +87,6 @@ export function ThreeDCardDemo({ id }) {
           </CardItem>
           <button
             onClick={handleClick}
-            translateZ={20}
             as="button"
             className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
           >
